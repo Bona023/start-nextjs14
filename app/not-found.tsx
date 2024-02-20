bb4a5814ac270b3fe7b6d9544202347b70cb8b0f
Bona_Kim <bona2324@gmail.com>
2024-02-21 04:00:06 +0900

    Initial commit

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import Navigation from "../components/navigation";
+
+export default function AboutUs() {
+    return (
+        <div>
+            <h1>Not Found - 404 ERROR!!</h1>
+        </div>
+    );
+}
